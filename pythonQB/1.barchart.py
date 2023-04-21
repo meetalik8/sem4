27358188ddfc1256dc1ac2a0b8875c8b8adefa6e
Meetali <102135323+meetalik8@users.noreply.github.com>
2023-04-21 08:44:42 +0530

    Update 1.barchart.py

--- pythonQB/1.barchart.py
+++ pythonQB/1.barchart.py
@@ -1,3 +1,6 @@
+#Write a Python programming to display a bar chart of the popularity of programming Languages.
+#Programming languages: Java, Python, PHP, JavaScript, C#, C++
+#Popularity: 22.2, 17.6, 8.8, 8, 7.7, 6.7
 import matplotlib.pyplot as plt
 x=['Java','Python','PHP','JS','C#','c++']
 y=[22.2,17.6,8.8,8,7.7,6.7]
