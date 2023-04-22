fa6c025dd9adc4048d0e16cc0f1d958b66414eb3
Meetali <102135323+meetalik8@users.noreply.github.com>
2023-04-22 11:37:28 +0530

    Create 3_array.py

---
+++ 3_array.py
@@ -0,0 +1,12 @@
+#ARRAYS
+import numpy as np
+
+a=np.array([20,21,22,23,24,25])
+print(a)
+print(type(a))
+print(len(a))
+print(a.ndim)
+b=np.array([[1,2,3],[4,5,6]])
+print(b.ndim)
+c=np.array([[[1,2,3],[4,5,6]],[[7,8,9],[10,11,12]]])
+print(c.ndim)
