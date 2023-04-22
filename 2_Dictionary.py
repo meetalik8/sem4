124d60e3a92c5f7fd73df0643ff45898f1f63d45
Meetali <102135323+meetalik8@users.noreply.github.com>
2023-04-22 11:24:36 +0530

    Create 2_Dictionary.py

---
+++ 2_Dictionary.py
@@ -0,0 +1,15 @@
+mydict={
+    "name":"Meetali",
+    "Roll no":65,
+    "Marks":"8.8"}
+print(mydict)
+print(mydict["name"])
+print(len(mydict))
+x= mydict.keys()
+print(x)
+mydict["Marks"]=89.8
+print(mydict)
+x= mydict.values()
+print(x)
+mydict["Marks"]=2.8
+print(mydict)
