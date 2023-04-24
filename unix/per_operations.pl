6df01683eddc3cd32b0422716cacf0b071f80353
Meetali <102135323+meetalik8@users.noreply.github.com>
2023-04-24 20:55:11 +0530

    Create per_operations.pl

---
+++ unix/per_operations.pl
@@ -0,0 +1,10 @@
+$summ = 2+4;
+$multt = 4*5;
+$divv = 10/2;
+$strr = "hello"."this is ok";
+
+print "Sum of 2 and 4  = $summ\n";
+print "Multiplication of 4 and 5= $multt\n";
+print "Division of 10 and 2=$divv\n";
+print "Concat of 2 strings = $strr\n";
+~                                       
