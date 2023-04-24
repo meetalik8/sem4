fae62170f40d50f224126353cc36731868da22b4
Meetali <102135323+meetalik8@users.noreply.github.com>
2023-04-24 11:58:17 +0530

    Create pandas_first3rows.py

---
+++ QB/pandas_first3rows.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as npexam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthe
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df = pd.DataFrame(exam_data , index=labels)
+print("First three rows of the data frame:")
+print(df.iloc[:3])
