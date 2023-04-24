3451abed55774d2278c3a9115ab8854b63dcdb2c
Meetali <102135323+meetalik8@users.noreply.github.com>
2023-04-25 04:41:21 +0530

    Update per_advpall.pl

--- unix/per_advpall.pl
+++ unix/per_advpall.pl
@@ -1,3 +1,4 @@
+#print weekday by index
 @weekdays = ('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday');
 print "Enter a number to print the weekday: ";
 $num = <>;
@@ -9,12 +10,14 @@ else {
         print "Invalid number\n";
 }
 
+#Print i/p data
 print "Enter your roll number :";
 $roll =<>;
 print "Enter your name: ";
 $name =<>;
 print "Name: $name\nRoll: $roll\n";
 
+#Check student data
 %students = (7,'Ella',13, 'Taylor', 22,'Jude');
 print "Enter a num: ";
 $n = <>;
@@ -25,9 +28,26 @@ else {
         print "Student data unavailable\n";
 }
 
+#Multiplication table
 print "Enter a number for table: ";
 $tn = <>;
 for ($i =1; $i<=10;$i++){
         $ans = int($tn*$i);
         print "$tn * $i = $ans\n";
+}
+
+#PRIME
+print "Enter a number to check for prime :";
+$p = <>;
+$c=0;
+for ($i=2;$i<=$p;$i++){
+        if($p%$i==0){
+                $count++;
+}
+}
+if($count>=2){
+        print"Not prime\n";
+}
+else {
+        print"Prime\n";
 }
