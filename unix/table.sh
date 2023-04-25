af8af1daf5817da15f43b1479d310e521e7b0780
Meetali <102135323+meetalik8@users.noreply.github.com>
2023-04-25 06:12:22 +0530

    Update table.sh

--- unix/table.sh
+++ unix/table.sh
@@ -1,8 +1,6 @@
-echo "Enter your roll number: "
-read ROLL
-echo "Power table of $ROLL"
-for((i=1;i<=5;i++))
-do
-        power=`expr $ROLL \* $i`
-        echo "$ROLL * $i = $power"
-done
+echo "Enter a number: "
+read NO
+for ((i=1;i<=5;i++)){
+        res=`expr $(($NO ** $i))`
+        echo "$NO raised to $i= $res"
+}
